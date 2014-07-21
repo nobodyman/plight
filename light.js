cc99354a21e98998a65b6723e328633c8ada577b
Jim deVos <jim.devos@gmail.com>
2014-07-20 20:33:41 -0700

    good news: no longer limited by setInterval,  bad news: 65hz seems to be the cap.
    I've changed it so that we don't try to change the color until the previous datagram has been sent.  However, because it's udp we don't know when the datagram is received,
    so you can still get into a situation where you flood the lifx with too many commands.  When lifx gets flooded it appears to ignore all incoming commands and so the color doesn't change.
    I've created a synchronous (blocking) function called waitFor()  so  to help wait for arbitrary amount of time for the lifx to receive the datagram and change the color.  I've played with the timings but it seems
    like you can't get lower than 15ms of waiting before flooding the lifx.
    
    other ideas to improve framerate:
    -  we could mimic ACK events  by sending the lifx a "gettime" or "getcolor"  command, and waiting for the response.   and then not send another color until we get the response from lifx. this might help us achieve the
    absolute fastest speed without flooding the lifx, but that fastest speed may just end up being 65hz.
    
    alternate ideas:
    - we can use the undocumented "pulseColor" command with "sawtooth" stepping  to effectively double the hz, but that might make drawing a bitmap super confusing.
    - similarly,  we could use pulseColor command with  "sine" stepping to create a a gradual transition between  hue=0 and hue=360 at hz.  This would create a "rainbow light"  when we use the spinner.

--- light.js
+++ light.js
@@ -1,12 +1,40 @@
 //you can't just require lifx.  you need to init the lib
 var lx = require("lifx").init();
+var packet = require("lifx/packet");
 
 //return the current epoch time 
 //todo: change this to use hrtime
-function getTime() {
+function getTimeOld() {
 	return (new Date()).getTime();
 }
 
+function getTime() {
+	var time = process.hrtime();
+	var nanos = time[0] * 1e9 + time[1];
+	var millis = parseInt(nanos / 1000000);
+	return millis;
+}
+
+//get microseconds. 1s == 1000ms == 1,000,000  micros
+function getMicrotime() {
+	var time = process.hrtime();
+	var nanos = time[0] * 1e9 + time[1];
+	var micros = parseInt(nanos / 1000);
+	return micros;
+
+}
+
+
+function waitFor(millis) {
+	var start = getTime();
+	var end = getTime();
+	while( (end - start) < millis) {
+		end = getTime();
+	}
+	//console.log("done waiting");
+}
+
+var started = false;
 
 //wait for a bulb to be found 
 lx.on("bulb", function(b1) {
@@ -20,6 +48,7 @@ lx.on("bulb", function(b1) {
 
 	//this function advances the light to the next color.  
 	var lightHue = function() {
+		started = true;
 		var hue = colors[(count % colors.length)];
 		lx.lightsColour(hue, 0xffff,     0xffff,    0xffff,   0, b1);
 		count++
@@ -35,7 +64,29 @@ lx.on("bulb", function(b1) {
 		console.log("colors cycled:%s  elapsedTime:%s  fps:%s", count, timeDiff.toFixed(2), (count/timeDiff).toFixed(2));
 	}
 
-	setInterval(lightHue, 1);
+	//waitFor(5000);
+	setTimeout(lightHue, 1000);
+
+//	console.log("time2: %s", getTime2());
+
+	lx.on("packetsent", function() {
+		//console.log("jimmy detected");
+		if(started) {
+			waitFor(15);
+			lightHue();
+		}
+	})
+
+	lx.on("rawpacket", function(pkt, rinfo) {
+		console.log("rawpacket:");
+		if(pkt.packetTypeShortName !== 'panGateway') {
+			console.dir(pkt);
+			
+		}
+		//console.dir(rinfo);
+		//lightHue();
+	});
+
 
 });
 
